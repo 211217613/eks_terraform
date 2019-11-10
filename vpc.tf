aa21a1be7dfd8762e796dc426ae69d489f99e775
Ruben Vazquez <rubenavazquez@gmail.com>
2019-11-10 12:21:46 -0500

    add aws_internet_gateway resource

--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "eks" {
 resource "aws_subnet" "demo" {
   count             = 2
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = "10.0.${count.index}.0.24"
+  cidr_block        = "10.0.${count.index}.0/24"
   vpc_id            = aws_vpc.eks.id
   tags = "${
     map(
@@ -24,4 +24,12 @@ resource "aws_subnet" "demo" {
     )
   }"
 
+}
+
+resource "aws_internet_gateway" "eks" {
+  vpc_id = aws_vpc.eks.id
+
+  tags = {
+    Name = "terraform-eks-demo"
+  }
 }
