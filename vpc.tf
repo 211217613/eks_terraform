aa7faf84e2006f0db733ad27866cdbf571428a52
Ruben Vazquez <rubenavazquez@gmail.com>
2019-11-10 12:14:41 -0500

    Add aws_vpc, aws_subnet resources

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "eks" {
+  cidr_block = "10.0.0.0/16"
+  tags = "${
+    map(
+      "Name", "terraform-eks-demo-node",
+      "kubernetes.io/cluster/${var.cluster-name}", "shared",
+
+    )
+  }"
+}
+
+resource "aws_subnet" "demo" {
+  count             = 2
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = "10.0.${count.index}.0.24"
+  vpc_id            = aws_vpc.eks.id
+}
